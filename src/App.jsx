5b09a0862f36b0f169bffd168d3c615d7f513daa
Nusfinal <nus_final@hotmail.com>
2024-12-02 20:28:18 +0700

    Add Project Store

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { About, Cart, Checkout, Error, HomeLayout, Landing, Login, Order, Product, Register, SingleProduct } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'product',
+        element: <Product />,
+      },
+      {
+        path: 'product/:id',
+        element: <SingleProduct />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'order',
+        element: <Order />,
+      },
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+])
+
+function App() {
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+
+}
+
+export default App
---
+++ src/pages/SingleProduct.jsx
@@ -0,0 +1,11 @@
+const singleProduct = () => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+  
+}
+
+export default singleProduct
